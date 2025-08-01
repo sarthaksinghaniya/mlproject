a3c2b1d08222aa27bee3540e3e2f27d73b89e6d9
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2025-08-01 18:03:35 +0530

    flask done yeahh

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request , redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
+# Config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -19,18 +20,48 @@ class Todo(db.Model):
         return f"{self.sno} - {self.title}"
 
 # Routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    todo = Todo(title = "First todo", desc = "start programing cutie")
-    db.session.add(todo)
-    db.session.commit()
-    return render_template('index.html')
+    if request.method == 'POST':
+        title = request.form.get('title')
+        desc = request.form.get('desc')
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
 
-@app.route('/products')
-def products():
+@app.route('/show')
+def show():
+    allTodos = Todo.query.all()
+    print(allTodos)
     return 'This is the products page'
 
-# Create database tables
+@app.route('/Delete/<int:sno>')
+def Delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/Update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if not todo:
+        return "Todo not found", 404
+    if request.method == 'POST':
+        title = request.form.get('title')
+        desc = request.form.get('desc')
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html', todo=todo)
+
+# ✅ Create DB inside context
 with app.app_context():
     db.create_all()
 
